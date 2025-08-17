f720f1c855cdcc323dcefe10d591b0e891418336
AlexDays89 <alejandromdiaz89@gmail.com>
2025-08-17 07:40:09 -0400

    fix cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,17 +2,24 @@ import express from "express";
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './swagger.js';
 import cors from "cors";
-import pool from "../schemas/db.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 import productosRoute from "./routes/productos.route.js";
 import authRoute from "./routes/auth.route.js";
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://proyecto-desafio-latam-rfqw.vercel.app",
+    "https://proyecto-desafiolatam-dev.onrender.com"
+];
+
 const app = express();
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(cors());
 app.use(express.json());
+
+
 
 app.get("/health", (_req, res) => res.send("ok"));
 
