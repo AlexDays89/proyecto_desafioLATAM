44761a7d5f83dc77e5fc7514991cf431eea8aaa5
AlexDays89 <alejandromdiaz89@gmail.com>
2025-08-17 14:16:17 -0400

    Actualizar Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -47,36 +47,33 @@ const Register = ({ onRegisterSuccess }) => {
     };
   
     try {
-      const response = await api('usuarios/register', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(userData),
-      });
-  
-      if (!response.ok) {
-        const data = await response.json();
-        setError(true);
-        setMensaje(data.error || 'Error en el registro');
-        return;
-      }
-  
-      const data = await response.json();
-      setUser(data.usuario);
-      if (typeof setToken === 'function' && data.token) {
-        setToken(data.token);
-      }
-      setError(false);
-      setMensaje('Registro Exitoso');
-      setTimeout(() => {
-        onRegisterSuccess();
-      }, 2000);
-      // Redirigir o limpiar formulario si quieres
-    } catch (error) {
-      console.error(error);
+    const response = await api('usuarios/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(userData),
+    });
+
+    if (response.error) {
       setError(true);
-      setMensaje('Error de conexión con el servidor');
+      setMensaje(response.error || 'Error en el registro');
+      return;
     }
-  };
+
+    setUser(response.usuario);
+    if (typeof setToken === 'function' && response.token) {
+      setToken(response.token);
+    }
+    setError(false);
+    setMensaje('Registro Exitoso');
+    setTimeout(() => {
+      onRegisterSuccess();
+    }, 2000);
+  } catch (error) {
+    console.error(error);
+    setError(true);
+    setMensaje('Error de conexión con el servidor');
+  }
+};
 
   return (
     <>
