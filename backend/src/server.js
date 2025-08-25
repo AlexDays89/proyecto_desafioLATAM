cef3449a93047c383bcc2e070a7a550c21a04582
AlexDays89 <alejandromdiaz89@gmail.com>
2025-08-25 19:17:09 -0400

    fix cors 4.0

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,12 +11,8 @@ import comprasRoute from "./routes/compras.route.js";
 
 const app = express();
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
-app.options('/productos', cors());
-app.options(/.*/, cors());
+app.use(cors());
+app.use(express.json());
 
 app.get("/health", (_req, res) => res.send("ok"));
 
